da083c10ee6a900d5cdc46b95d1c52197b10cb15
James Jones <jj98910@email.vccs.edu>
2026-03-09 08:26:53 -0400

    1st option

--- projects/game/game.js
+++ projects/game/game.js
@@ -1,55 +1,11 @@
-let gameActive = true; //this variable is required. 
-                       //to stop the game, set it to false.
+// This variable keeps track of which room you are in.
+// We start at "start".
+let currentRoom = "start";
+// This function runs every time you click a button.
+// 'choice' will be 1 if you click the left button, and 2 if you click the right.
+if (currentRoom === "start") {
+        if (choice === 1) {
+		currentRoom = "cafe";
+print("You leave Oxygen Storage and enter the Coral Cafe. It smells like salt. Go to the Central Hub?", "Go to Central Hub", "Stay Here");
 
-//Declare your other global variables here
 
-
-//If you need, add any "helper" functions here
-
-
-//Make one function for each location
-function locationA() {
-    clear();
-    print("\nYou are in location A!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
-    
-    function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
-        } else {
-            stayHere();
-            waitThenCall(locationA);
-        }
-    }
-    waitForInput(processInput);
-}
-
-function locationB() {
-    clear();
-    print("\nYou are in location B!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
-    
-    function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
-        } else {
-            stayHere();
-            waitThenCall(locationB);
-        }
-    }
-    waitForInput(processInput);
-}
-
-//finally, make sure you customize this to tell it what should happen at the
-//very start. For this simple example, any input will bring you
-//to locationA
-function start(){
-    print("Welcome to my game! Press any key to start");
-
-    function processInput(input){
-            locationA();
-    }
-    waitForInput(processInput);
-}
