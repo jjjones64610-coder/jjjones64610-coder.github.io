4e9db7420886b5eb257ebe7371d4904cde74e863
James Jones <jj98910@email.vccs.edu>
2026-03-11 08:58:43 -0400

    i added the ending

--- projects/game/game.js
+++ projects/game/game.js
@@ -19,4 +19,30 @@ print("You leave Oxygen Storage and enter the Coral Cafe. It smells like salt. G
         } else {
             gameOver("You waited too long and your air tank ran out. Game Over!");
         }
+    }
+else if (currentRoom == "central") {
+        if (choice == 1) {
+            currentRoom = "tunnel";
+		print("You are in the glass Shark Tunnel. It is cracking! Run to the Control Shed?", "Run to Shed", "Go Back");
+        } else {
+            gameOver("You tried to hide, but the room flooded. Game Over!");
+        }
+    }
+else if (currentRoom == "tunnel") {
+        if (choice == 1) {
+            currentRoom = "shed";
+            print("You made it to the Control Shed! There are two doors. Which one do you open?", "Hatch 511", "Pump Room");
+        } else {
+            gameOver("The glass broke before you could run back! Game Over!");
+        }
+    }
+else if (currentRoom == "shed") {
+        if (choice == 1) {
+            winGame("You climbed through Hatch 511 and reached the surface! YOU ESCAPED!");
+        } else {
+		gameOver("The Pump Room was a trap and it flooded instantly! Game Over!");
+		}
+    }
+}
+
 
